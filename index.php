e3a64349de4727905924cfc03ab5be90640dda4f
Ariel Torreon <134142189+Ayeh93@users.noreply.github.com>
2023-06-05 23:02:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/ayeh-icon.ico">
+    <!-- google fonts -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <!-- font-family: 'Roboto Condensed', sans-serif; -->
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;700&display=swap" rel="stylesheet">
+    <!-- font-family: 'Oxygen', sans-serif; -->
+    <link rel="stylesheet" type="text/css" href="sass/style.css">
+    <!-- Linkedin Card End -->
+    <title>Ariel Torreon - Portfolio</title>
+</head>
+
+<body>
+
+</body>
+<nav class="site-header">
+    <div class="nav-logo">
+        <a href="index.html">
+            <img src="assets/images/logo-new.png" alt="logo" width="314" height="60">
+        </a>
+    </div>
+    <button class="hamburger"><img src="assets/images/menu.png" alt="menu toggle icon"></button>
+    <div class="nav-links">
+        <ul>
+            <li><a href="#home" class="active">Home</a></li>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#skill">Skills</a></li>
+            <li><a href="#service">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+</nav><!-- .site-header -->
+
+<div class="avatar">
+    <img src="assets/images/ayeh.png" alt="Ariel, headshot">
+</div>
+<section class="banner" id="home">
+    <div class="banner--content">
+        <h2 class="name">Ariel Torreon</h2>
+        <h1 class="designation">Web Developer/Programmer</h1>
+        <p class="tagline">We live and breathe software development.</p>
+    </div>
+</section><!-- .banner -->
+
+<section id="about">
+    <div class="content">
+        <h2>About Me</h2>
+        <div class="about-container">
+            <div class="about-container-left">
+                <p>I am a Software developer with more than 5 years of relevant job experience in developing Web-Based
+                    and Desktop
+                    Application Software. Providing the agency with a more comfortable, easy, and fast way of making
+                    emergency or
+                    on-the-spot report gathering. Working with a team with minimal supervision, A creative thinker,
+                    adept in software
+                    development and working with various data structures.</p>
+                <h3>Accomplishments</h3>
+                <ul>
+                    <li>Served as Lead Programmer on Digital Document Tracking vb.net.</li>
+                    <li>Build Barangay Management System using vb.net that allows user to create, manage, and generate
+                        fast document as needed.</li>
+                    <li>Build Web-Base Procurement System using PHP, MySql, CSS, HTML, JavaScript</li>
+                    <li>Build Attendance Monitoring System using c# this allows user to login using camera and automate
+                        document needed for reporting.</li>
+                </ul>
+                <div class="item">
+                    <a target="_blank"
+                        href="https://drive.google.com/file/d/1LihQ95ODvbGJJ8Y8LgEr2Fg0ZCvzs5FX/view?usp=sharing">My
+                        Resume</a>
+                </div>
+                <!--<h5>Computer Programmer, PLGU-Dinagat Islands</h5>
+			  <ul class="work">
+				<li>&#10003; Build Web-Based Systems to enable agency related, to digitalized the work for more efficient and faster way of giving day to day report.</li>
+				<li>&#10003; Built, tested and deployed scalable, highly available and modular software products.</li>
+				<li>&#10003; Consulted regularly with internal customers on application development project status, new project proposals and software-related technical issues.</li>
+				<li>&#10003; Analyzed and designed relational databases using XAMPP.</li>
+				<li>&#10003; Performed regression and system-level testing to verify software quality and function prior to release.</li>
+				<li>&#10003; Tested hardware using Visual Studio Development tools and performed troubleshooting techniques to identify problems.</li>
+			  </ul> -->
+            </div>
+            <!-- <div class="about-container-right">
+				<div class="avatar">
+					<img src="assets/images/ayeh.png" alt="Ariel, headshot">
+				</div>
+			</div> -->
+        </div>
+    </div>
+</section><!-- #about -->
+<section id="skill">
+    <div class="content skill--content">
+        <div class="left">
+            <h2>My skills & expertise</h2>
+            <p>Some skills gain for the programming experience.</p>
+        </div>
+        <div class="right">
+            <div class="item">
+                <img src="assets/images/photoshop.png" alt="Photoshop">
+                <h3>Abode Photoshop</h3>
+                <p>Designing site banner or any related images to the site.</p>
+            </div>
+            <div class="item">
+                <img src="assets/svg/PHP-logo.svg" alt="PHP">
+                <h3>PHP</h3>
+                <p>Programming language I use to develop Web-Based Systems.</p>
+            </div>
+            <div class="item">
+                <img src="assets/images/js.png" alt="JavaScript">
+                <h3>JavaScript</h3>
+                <p>JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies
+                    of the World Wide Web, alongside HTML and CSS.</p>
+            </div>
+            <div class="item">
+                <img src="assets/images/css.png" alt="CSS">
+                <h3>CSS stylesheet</h3>
+                <p>CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.</p>
+            </div>
+            <div class="item">
+                <img src="assets/images/html.png" alt="HTML">
+                <h3>HTML</h3>
+                <p>The HyperText Markup Language or HTML is the standard markup language for documents designed to be
+                    displayed in a web browser.</p>
+            </div>
+            <div class="item">
+                <img src="assets/images/csharp.png" alt="C#">
+                <h3>C#</h3>
+                <p>C# is a general-purpose high-level programming language supporting multiple paradigms.</p>
+            </div>
+            <div class="item">
+                <img src="assets/images/vb-net.png" alt="C#">
+                <h3>VB.net</h3>
+                <p>C# is a general-purpose high-level programming language supporting multiple paradigms.</p>
+            </div>
+        </div>
+    </div>
+</section><!-- #skill -->
+<section id="service">
+    <h2>Services I offer</h2>
+    <div class="content service--content">
+        <div class="item">
+            <h3>Software Development</h3>
+            <img src="assets/images/software.png" alt="Logo design">
+            <p>!</p>
+            <a href="#contact">Hire Me</a>
+        </div><!-- .item -->
+        <div class="item">
+            <h3>Web Development</h3>
+            <img src="assets/images/web development.jpg" alt="Packaging design">
+            <p>.</p>
+            <a href="#contact">Hire Me</a>
+        </div><!-- .item -->
+        <div class="item">
+            <h3>Web-Based Systems</h3>
+            <img src="assets/images/web.png" alt="Packaging design">
+            <p>.</p>
+            <a href="#contact">Hire Me</a>
+        </div><!-- .item -->
+        <div class="item">
+            <h3>Photoshop</h3>
+            <img src="assets/images/photoshop.png" alt="Packaging design">
+            <p>.</p>
+            <a href="#contact">Hire Me</a>
+        </div><!-- .item -->
+    </div>
+</section><!-- #service -->
+
+<section id="portfolio">
+    <h2 class="center">Portfolio -My Development Sample</h2>
+    <div class="content portfolio--content">
+        <div class="item">
+            <h3>Web-Base Procurement System (PHP, Boostrap)</h3>
+            <img src="assets/images/procurement.png" alt="logo design">
+        </div><!-- item -->
+        <div class="item">
+            <h3>Web-Base Attendance Monitoring (PHP, Boostrap)</h3>
+            <img src="assets/images/Attendance.png" alt="logo design">
+        </div><!-- item -->
+        <div class="item">
+            <h3>Document Tracking (vb.net)</h3>
+            <img src="assets/images/Tracking.png" alt="logo design">
+        </div><!-- item -->
+        <div class="item">
+            <h3>Barangay Management System (vb.net)</h3>
+            <a target="_blank"
+                href="https://drive.google.com/drive/folders/1QCMPX-d1ZjYsLbMr09ssp8pc6WkDetK2?usp=sharing">
+                <img src="assets/images/BMS.png" alt="logo design"></a>
+        </div><!-- item -->
+        <div class="item">
+            <h3>Attendance Monitoring System (c#)</h3>
+            <a target="_blank"
+                href="https://drive.google.com/drive/folders/1cjmu0A3XE9EKC32vsd8JdlkP5dlaDkZ3?usp=sharing">
+                <img src="assets/images/attendance-camera.png" alt="logo design"></a>
+        </div><!-- item -->
+    </div>
+</section><!-- portfolio -->
+
+<section id="contact">
+    <div class="content">
+        <h2 class="center">Contact Me</h2>
+        <address>
+            <p>Email Me: <a href="mailto:arieldivinetorreon@gmail.com">arieldivinetorreon@gmail.com</a></p>
+            <p>Call me: <a href="tel:+639151303578">+639151303578</a></p>
+            <p>Address: P3 San Juan, San Jose, Dinagat Islands.</p>
+        </address>
+        <!-- <div class="container">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Your name..">
+
+                <label for="email">Email</label>
+                <input type="text" id="email" name="from" placeholder="Your email..">
+
+                <label for="subject">Subject</label>
+                <input type="text" id="subject" name="subject" placeholder="Subject..">
+
+                <label for="message">Message</label>
+                <textarea id="message" name="message" placeholder="Write something.." style="height:200px"></textarea>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div> -->
+    </div>
+
+</section>
+<footer class="site-footer">
+    <div class="copyright-notice">
+        <p>© <time datetime="2023"><?=$retVal = (date("Y")=="2023") ? "2023" : "2023-".date('Y') ;?>,</time> All rights
+            reserved by ArielTorreon</p>
+    </div>
+</footer>
+<div class="social-share">
+    <a href="https://www.facebook.com/ayehtorreon93" target="_blank"><img src="assets/svg/facebook.svg"
+            alt="share on facebook"></a>
+    <a href="https://www.onlinejobs.ph/jobseekers/info/2521157" target="_blank"><img src="assets/svg/oj-logo-icon.svg"
+            alt="share on linkedin"></a>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+</html>
